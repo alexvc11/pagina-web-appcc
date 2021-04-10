888c28cd11f61b6c016333f131182f82953cc184
alexvc11 <61017171+alexvc11@users.noreply.github.com>
2021-04-10 13:35:52 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+
+const form = document.getElementById("form-datos");
+var num_preg = document.getElementById("INnum");
+const tit_preg = document.getElementById("INpreg");
+const opc_A = document.getElementById("INopc_A");
+const opc_B = document.getElementById("INopc_B");
+const opc_C = document.getElementById("INopc_C");
+const opc_D = document.getElementById("INopc_D");
+const txta = document.getElementById("ta-resp-correcta");
+const body = document.getElementById("body");
+
+window.onload = () => {
+    const contraseña = window.prompt("Contraseña:",);
+    //TODO: comprobar contraseña a través de una variable de entorno en el servidor web
+    //REVISAR: poner el texto de la contraseña con arteriscos.
+    if(contraseña != "1234"){
+        window.close();
+    }
+    
+}
+form.onsubmit = (e) => {
+    //TODO: agregar implementación con la base de datos.
+
+    num_preg.value = 1;
+    tit_preg.value = "";
+    opc_A.value = "";
+    opc_B.value = "";
+    opc_C.value = "";
+    opc_D.value = "";
+    txta.value = "";
+
+    e.preventDefault();
+}
+
